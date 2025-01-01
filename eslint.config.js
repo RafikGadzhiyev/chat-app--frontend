592f325446d4a7df28738c1f1c01fb27eb3d41bc
Rafik <rafik.gadzhiyev.03@bk.ru>
2025-01-01 23:17:13 +0500

    chore(eslint): update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,92 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      'quotes': 'warn'
+      'quotes': 'warn',
+      'newline-before-return': 'warn',
+      'no-lonely-if': 'error',
+
+      'array-bracket-newline': ['off', 'consistent'],
+      'array-bracket-spacing': ['warn', 'never'],
+      'block-spacing': ['warn', 'always'],
+      'function-call-argument-newline': ['warn', 'consistent'],
+      'func-call-spacing': ['warn', 'never'],
+      'function-paren-newline': ['warn', 'multiline-arguments'],
+      'indent': ['warn', 2],
+      'jsx-quotes': ['warn', 'prefer-double'],
+      'max-depth': ['error', 4],
+      'no-multi-assign': ['error'],
+
+      'array-element-newline': [
+        'off',
+        {
+          multiline: true,
+          minItems: 2
+        }
+      ],
+
+      'brace-style': [
+        'warn',
+        '1tbs',
+        {
+          allowSingleLine: true
+        }
+      ],
+
+      'comma-dangle': [
+        'warn',
+        {
+          arrays: 'always-multiline',
+          objects: 'always-multiline',
+          imports: 'always-multiline',
+          exports: 'always-multiline',
+          functions: 'always-multiline',
+        }
+      ],
+
+      'comma-spacing': [
+        'warn',
+        {
+          before: false,
+          after: true
+        }
+      ],
+
+      'key-spacing': [
+        'error',
+        {
+          beforeColon: false,
+          afterColon: true
+        }
+      ],
+
+      'max-len': [
+        'warn',
+        100,
+        2,
+        {
+          ignoreUrls: true,
+          ignoreComments: false,
+          ignoreRegExpLiterals: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+        }
+      ],
+
+      'newline-per-chained-call': [
+        'error',
+        {
+          ignoreChainWithDepth: 1
+        }
+      ],
+
+      'no-multiple-empty-lines': [
+        'warn',
+        {
+          max: 1,
+          maxBOF: 0,
+          maxEOF: 0
+        }
+      ],
     },
   },
 )

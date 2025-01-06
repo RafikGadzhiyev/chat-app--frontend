52ca27693dc55fef824cc52294fe22037092ae52
Rafik <rafik.gadzhiyev.03@bk.ru>
2025-01-06 23:18:37 +0500

    refactor(ui): split component logic

--- src/components/dialogs/CreateChatDialog.tsx
+++ src/components/dialogs/CreateChatDialog.tsx
@@ -1,57 +1,29 @@
-import {useState} from "react";
+import { useState } from "react";
 
 import {Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger} from "@/components/ui/Dialog/Dialog.tsx";
 import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/Tooltip/Tooltip.tsx";
 import {Icon} from "@/components/ui/Icon/Icon.tsx";
-import {Input} from "@/components/ui/Input/Input.tsx";
-import {Button} from "@/components/ui/Button/Button.tsx";
+import ChatCreationForm from "@/components/forms/ChatCreationForm.tsx";
 
-import { useLoading } from "@/hooks/useLoading"
-
-import {showErrorToast} from "@/utils/toast.util.ts";
-
-import api from "@/server";
-import Loading from "@/components/ui/Loading/Loading.tsx";
+import {Chat} from "@/types.ts";
 
 interface ICreateChatDialog {
-  onChatCreate: (chat: any) => void
+  onChatCreate: (chat: Chat) => void
 }
 
 export default function CreateChatDialog(props: ICreateChatDialog) {
-  const [ title, setTitle ] = useState("")
-  const [ description, setDescription ] = useState("")
+  const [isOpen, setIsOpen] = useState(false)
 
-  const {
-    isLoading,
-    startLoading,
-    stopLoading,
-  } = useLoading()
-
-  function createNewChat() {
-    startLoading()
-
-    api.chat.create(
-      {
-        title: title,
-        description: description,
-        memberEmails: [
-          "1@mail.ru"
-        ],
-      }
-    )
-      .then(createdChat => {
-        props.onChatCreate(createdChat)
-      })
-      .catch(err => {
-        console.error(err)
-        showErrorToast(err.message)
-      })
-      .finally(stopLoading)
+  function closeDialog() {
+    setIsOpen(false)
   }
 
   return (
-    <>
-      <Dialog>
+    <div>
+      <Dialog
+        open={isOpen}
+        onOpenChange={setIsOpen}
+      >
         <DialogTrigger>
           <TooltipProvider>
             <Tooltip>
@@ -81,31 +53,13 @@ export default function CreateChatDialog(props: ICreateChatDialog) {
           </DialogHeader>
 
           <div className='flex flex-col gap-4'>
-            <Input
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder='Title'
+            <ChatCreationForm
+              onChatCreate={props.onChatCreate}
+              closeDialog={closeDialog}
             />
-
-            <Input
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              placeholder='Description'
-            />
-
-            <Button
-              onClick={createNewChat}
-              variant='outline'
-            >
-              Create
-            </Button>
           </div>
         </DialogContent>
       </Dialog>
-
-      <Loading
-        show={isLoading}
-      />
-    </>
+    </div>
   )
 }
